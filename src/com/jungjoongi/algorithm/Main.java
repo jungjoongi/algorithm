8200e07686c93f7eae1018d23a8cb9200781c9e3
jungjoongi <dev.joongi@gmail.com>
2020-02-12 22:00:19 +0900

    algorithm

---
+++ src/com/jungjoongi/algorithm/Main.java
@@ -0,0 +1,9 @@
+package com.jungjoongi.algorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("This is Algorithm project");
+    }
+}
