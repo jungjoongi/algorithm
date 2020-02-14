7fddc3431a1482790a571b0748a155829f88c913
jungjoongi <dev.joongi@gmail.com>
2020-02-14 23:59:08 +0900

    array

---
+++ src/com/jungjoongi/algorithm/step01/Array.java
@@ -0,0 +1,43 @@
+package com.jungjoongi.algorithm.step01;
+
+public class Array {
+
+    /*
+        배열 swap 알고리즘
+     */
+
+    public static void main(String[] args) {
+    	
+    	int[] beforeSwapArray = {1,6,2,3,5,12,53,7234,123,5,12};
+    	System.out.print("befor :: ");
+    	for(int i = 0; i < beforeSwapArray.length; i++) {
+    		System.out.print(beforeSwapArray[i] + " ");
+    	}
+    	System.out.println("\n");
+    	
+    	System.out.print("after :: ");
+    	int[] afterSwapArray = beforeSwapArray;
+    	reverseArray(afterSwapArray);
+    	for(int i = 0; i < afterSwapArray.length; i++) {
+    		System.out.print(afterSwapArray[i] + " ");
+    	}
+    	System.out.println("\n");
+
+    }
+    
+    
+    static void reverseArray(int[] arr) {
+    	int length = arr.length;
+    	for (int i = 0; i < length / 2; i++) {
+    		swap(arr, i, length-1);
+    	}
+    }
+    
+    static void swap(int[] arr, int cursor, int length) {
+    	int temp = arr[cursor];
+    	arr[cursor] = arr[length-cursor];
+    	arr[length-cursor] = temp;
+    }
+
+    
+}
